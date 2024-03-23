b4fd9bc8de0e306a47097b52045ad9db4d72b58c
Sergio Suárez <sergiosuarezcamacho@gmail.com>
2024-03-23 20:02:38 +0100

    Figuras creadas

---
+++ Figuras/src/Model/Model.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Model {
+
+	public static void main(String[] args) {
+		Circulo c1 = new Circulo();
+		Circulo c2 = new Circulo(3);
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo(3, 5);
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(r1);
+		System.out.println(r2);
+
+		
+		
+		
+	}
+
+}
